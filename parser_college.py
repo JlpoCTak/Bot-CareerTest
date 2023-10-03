886559e08afa653558669395f65ffdd621535557
JlpoCTak <JlpocTaK@yandex.ru>
2023-10-03 13:29:46 +0300

    1

--- parser_college.py
+++ parser_college.py
@@ -1,3 +1,5 @@
+import functools
+
 from config import url
 import mechanicalsoup
 
@@ -11,18 +13,27 @@ def write_data_in_file(page):
     specs = page.find_all('ul', id = 'specs')
     for spec in specs:
         s = spec.find_all('li')
+        j=0
+        print(s[0].getText())
+        with open('specs.txt', 'w', encoding='utf-8') as file:
+            for i in range(620):
+                kod = (s[i].find_all('span'))
+                name = (s[i].find_all('a'))
+
+                if s[i].find_all('span')==[]:
+                    print(f'{j+1}) {s[i].getText()} \n ')
+                    file.write(f'{j+1}) {s[i].getText()} \n')
+                    j+=1
+                else:
+
+                    for text_name in name:
+
+                        for text_kod in kod:
+                            file.write(f'--  {text_kod.getText()} - {text_name.getText()} \n')
+                            print(text_kod.getText(), ' - ', text_name.getText())
 
 
-        if s[0].find_all('span')==[]:
-            qwe = (s[1].find_all('span'))
-            for data in qwe:
-                print(data.getText())
-        else:
-            continue
-        if s[0].find_all('a')==[]:
-            qwe = (s[1].find_all('a'))
-            for data in qwe:
-                print(data.getText())
+
 
 
 
