d8cc00df8795acbe48e2d97a0aec6861f6ff22d1
MADoMAXs <146708097+MADoMAXs@users.noreply.github.com>
2023-10-06 21:53:46 +0300

    1
    1

--- Keyboard.py
+++ Keyboard.py
@@ -4,5 +4,19 @@ from aiogram.types import ReplyKeyboardRemove, \
 
 button_hi = KeyboardButton('Привет!')
 
-greet_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_hi)
+greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_key=True).add(button_hi)
 greet_kb.add(button_hi)
+
+button1 = KeyboardButton('1')
+button2 = KeyboardButton('2')
+button3 = KeyboardButton('3')
+button4 = KeyboardButton('4')
+button5 = KeyboardButton('5')
+
+inline_btn_1 = InlineKeyboardButton('Привет', callback_data='Button_hi')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
+inline_kb_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_1)
+inline_kb_full.add(InlineKeyboardButton('1', callback_data='btn1'))
+inline_btn_3 = InlineKeyboardButton('2', callback_data='btn2')
+inline_btn_4 = InlineKeyboardButton('3', callback_data='btn3')
+inline_btn_5 = InlineKeyboardButton('4', callback_data='btn4')
--- handlers.py
+++ handlers.py
@@ -1,14 +1,21 @@
 from aiogram import types, F, Router
+from aiogram import Bot
 from aiogram.types import Message
 from aiogram.filters import Command
 import Keyboard as kb
 
+bot = Bot()
 router = Router()
 
 @router.message(Command("start"))
 async def process_start_command (msg: Message):
-    await msg.answer("Привет! Я помогу тебе подобрать специальность, которая будет соответствовать твоим интересам, просто остваь мне любое сообщение", reply_markup=kb.greet_kb)
+    await msg.answer("Привет! Я помогу тебе подобрать специальность, которая будет соответствовать твоим интересам, просто остваь мне любое сообщение", reply_markup=kb.inline_kb1)
 
-@router.message()
+
+@router.callback_query_handlers(func=lambda c: c.data == 'button_hi')
+async def process_callback_button_hi(callback_query: types.CallbackQuery.id):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, 'Специально для тебя, я подготовил профориентационный тест, оствь мне сообщение, чтобы продолжить')
+@router.messeage()
 async def message_handler(msg: Message):
     await msg.answer("Специально для тебя, я подготовил профориентационный тест, оствь мне сообщение, чтобы продолжить")
