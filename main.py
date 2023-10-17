1eeeb6b7292f3c8353a1a87a124857eb122ef274
MADoMAXs <146708097+MADoMAXs@users.noreply.github.com>
2023-10-17 21:44:13 +0300

    1
    1

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import asyncio
 import logging
 import sys
 
-
-
 from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
-
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from handlers import router
+import config
 
 dp = Dispatcher()
 TOKEN = '6680356595:AAGRMtLUHRBXUTXfJIv9qFhC3a53asrTmnY'
 async def main():
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    await dp.start_polling(bot)
+    bot = Bot(token=config.Bot_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
