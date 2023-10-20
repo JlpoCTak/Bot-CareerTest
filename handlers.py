03d8b14dc941d310d862080801587736335cf1ea
MADoMAXs <146708097+MADoMAXs@users.noreply.github.com>
2023-10-20 20:41:17 +0300

    1
    1

--- handlers.py
+++ handlers.py
@@ -25,26 +25,34 @@ async def start_handler(msg: Message):
                      reply_markup=builder.as_markup()
     )
 
+
+
 @router.callback_query(F.data == 'Test')
 async def Test(callback: types.CallbackQuery):
-    with open('database/professions_for_text.json', 'r', encoding='utf-8') as profissions_text:
+    with open('database/professions_for_text.json', 'r', encoding='utf-8') as professions_text:
         with open('database/holland_table.json', 'r', encoding='utf-8') as holland_table:
             dict_prof = {'realistic': 0, 'intelligent': 0, 'social': 0, 'conventional': 0, 'enterprising': 0,
                          'artistic': 0}
             max_ball_group = []
-            professions = json.load(profissions_text)
+            professions = json.load(professions_text)
             hol_table = json.load(holland_table)
             list_prof = list(professions.keys())
             for i in range(42):
-                builder = InlineKeyboardBuilder()
-                builder.add(types.InlineKeyboardButton(
-                    text=f"{list_prof[i*2]}",
-                    callback_data="answer_a")
+                btn1 = InlineKeyboardButton(
+                    text=f'{list_prof[i * 2]}',
+                    callback_data='answer_a'
                 )
-                types.InlineKeyboardButton(text=f"{list_prof[i*2+1]}",callback_data='answer_b')
-                await callback.message.answer(f"1){list_prof[i*2]} - {professions[f'{list_prof[i*2]}']}"
-                                              f"\n2){list_prof[i*+1]} - {professions[f'{list_prof[i*2+1]}']}")
-                
+                btn2 = InlineKeyboardButton(
+                    text=f'{list_prof[i * 2+1]}',
+                    callback_data='answer_b'
+                )
+                keyboard = InlineKeyboardMarkup(
+                    inline_keyboard=[[btn1],
+                                     [btn2]]
+                )
+                await callback.message.answer(f"1){list_prof[i*2]} - {professions[f'{list_prof[i*2]}']} "
+                                              f"\n2){list_prof[i*+1]} - {professions[f'{list_prof[i*2+1]}']}", reply_markup=keyboard)
+
                 # print(F.callback_data)
                 # print(InlineKeyboardButton.callback_data)
             #     answer = f'{i + 1}'# + InlineKeyboardButton.callback_data
