62a910332346a12890c5d1f20835a62ebdad5a1a
JlpoCTak <JlpocTaK@yandex.ru>
2023-10-20 15:15:00 +0300

    work bad

--- handlers.py
+++ handlers.py
@@ -1,3 +1,5 @@
+import time
+
 from aiogram import types, F, Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
@@ -24,7 +26,7 @@ async def start_handler(msg: Message):
     )
 
 @router.callback_query(F.data == 'Test')
-async def Test(msg: Message):
+async def Test(callback: types.CallbackQuery):
     with open('database/professions_for_text.json', 'r', encoding='utf-8') as profissions_text:
         with open('database/holland_table.json', 'r', encoding='utf-8') as holland_table:
             dict_prof = {'realistic': 0, 'intelligent': 0, 'social': 0, 'conventional': 0, 'enterprising': 0,
@@ -39,8 +41,10 @@ async def Test(msg: Message):
                     text=f"{list_prof[i*2]}",
                     callback_data="answer_a")
                 )
-                # types.InlineKeyboardButton(text=f"{list_prof[i*2+1]}",callback_data='answer_b')
-                await msg.answer(f"1){list_prof[i*2]} - {professions[f'{list_prof[i*2]}']}")
+                types.InlineKeyboardButton(text=f"{list_prof[i*2+1]}",callback_data='answer_b')
+                await callback.message.answer(f"1){list_prof[i*2]} - {professions[f'{list_prof[i*2]}']}"
+                                              f"\n2){list_prof[i*+1]} - {professions[f'{list_prof[i*2+1]}']}")
+                
                 # print(F.callback_data)
                 # print(InlineKeyboardButton.callback_data)
             #     answer = f'{i + 1}'# + InlineKeyboardButton.callback_data
