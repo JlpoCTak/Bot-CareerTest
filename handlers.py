f30b1e8ee5c8eb9b3eca96efb81b60f168e5fe9b
MADoMAXs <146708097+MADoMAXs@users.noreply.github.com>
2023-10-21 22:10:13 +0300

    1
    1

--- handlers.py
+++ handlers.py
@@ -6,12 +6,18 @@ from aiogram.filters import Command, CommandStart
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import aiogram.filters.callback_data as filters
 from aiogram.utils.markdown import hbold
+from aiogram.fsm.context import FSMContext
+from aiogram.filters.state import State, StatesGroup
 import json
 
 from kb import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 import text
 # from prof_test import test_holland
 router = Router()
+
+
+
+
 
 
 @router.message(Command("start"))
@@ -25,8 +31,6 @@ async def start_handler(msg: Message):
                      reply_markup=builder.as_markup()
     )
 
-
-
 @router.callback_query(F.data == 'Test')
 async def Test(callback: types.CallbackQuery):
     with open('database/professions_for_text.json', 'r', encoding='utf-8') as professions_text:
@@ -38,6 +42,10 @@ async def Test(callback: types.CallbackQuery):
             hol_table = json.load(holland_table)
             list_prof = list(professions.keys())
             for i in range(42):
+                class Order_answer(StatesGroup):
+                    choosing_option1 = State(f'{list_prof[i * 2]}')
+                    choosing_option2 = State(f'{list_prof[i * 2+1]}')
+
                 btn1 = InlineKeyboardButton(
                     text=f'{list_prof[i * 2]}',
                     callback_data='answer_a'
@@ -51,7 +59,9 @@ async def Test(callback: types.CallbackQuery):
                                      [btn2]]
                 )
                 await callback.message.answer(f"1){list_prof[i*2]} - {professions[f'{list_prof[i*2]}']} "
-                                              f"\n2){list_prof[i*+1]} - {professions[f'{list_prof[i*2+1]}']}", reply_markup=keyboard)
+                                              f"\n2){list_prof[i*+1]} - {professions[f'{list_prof[i*2+1]}']}", reply_markup=keyboard,)
+                await State.set_state(Order_answer.choosing_option1.state)
+                await State.set_state(Order_answer.choosing_option2.state)
 
                 # print(F.callback_data)
                 # print(InlineKeyboardButton.callback_data)
