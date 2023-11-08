8b7267457e8ca1574d61d9f943455a41e9cf0bec
JlpoCTak <JlpocTaK@yandex.ru>
2023-11-08 08:38:55 +0300

    .

--- handlers.py
+++ handlers.py
@@ -5,7 +5,7 @@ from aiogram import types, F, Router, Bot, Dispatcher
 from aiogram.handlers import message
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 import aiogram.filters.callback_data as filters
 from aiogram.utils.markdown import hbold
 from aiogram.fsm.context import FSMContext
@@ -28,6 +28,8 @@ class WaitList(StatesGroup):
     answer_question = State()
     start = State()
     passed_test = State()
+    change_city = State()
+    menu = State()
 
 
 @router.message(Command("start"))
@@ -50,6 +52,7 @@ async def start_handler(msg: Message, state: FSMContext):
     dict_num_ques = {}
     dict_num_ques['number_question'] = 0
     await state.set_data(dict_num_ques)
+    await msg.delete()
 
 
 @router.callback_query(F.data == 'Test')
@@ -80,22 +83,26 @@ async def ask_questions(callback: types.CallbackQuery, state: FSMContext):
                                           f"\n2){list_prof[number_question * +1]} - {professions[f'{list_prof[number_question * 2 + 1]}']}",
                                           reply_markup=keyboard, )
 
-
-
             print(callback.data)
             print('number question', number_question+1)
     elif number_question > 41:
         await callback.message.answer('Вы прошли тест, теперь введите город в котором вы хотите обучаться')
-        await state.set_state(WaitList.passed_test)
+        await state.set_state(WaitList.change_city)
 
 
-@router.message(WaitList.passed_test, F.text == 'Город')
+@router.message(WaitList.change_city)
 async def take_city(msg: Message, state: FSMContext):
     city = msg.text
     user_id = msg.from_user.id
     with sqlite3.connect('database/users.db') as connection:
         cursor = connection.cursor()
         cursor.execute('''UPDATE users_answer SET city = ? WHERE tg_user_id = ?''', (city, user_id))
+    await state.set_state(WaitList.menu)
+    bot_commands = [
+        types.BotCommand(command="/menu", description="меню действий"),
+
+    ]
+    await bot.set_my_commands(bot_commands)
 
 
 
@@ -143,8 +150,8 @@ async def answer_b(callback: types.CallbackQuery, state: FSMContext):
     await callback.message.answer('Перейти к следущему вопросу', reply_markup=keyboard.as_markup())
 
 
-@router.message(F.text == 'Тест') #WaitList.passed_test,
-async def final(msg: Message, state: FSMContext):
+@router.callback_query(F.data == 'result_test') #WaitList.passed_test,
+async def final(callback: types.CallbackQuery, state: FSMContext):
     with open('database/holland_table.json', 'r', encoding='utf-8') as holland_table:
         with sqlite3.connect('database/users.db') as connection:
             cursor = connection.cursor()
@@ -153,7 +160,7 @@ async def final(msg: Message, state: FSMContext):
                          'artistic': 0}
             max_ball_group = []
             hol_table = json.load(holland_table)
-            tg_user_id = msg.from_user.id
+            tg_user_id = callback.from_user.id
             answer_list = []
             for i in range(42):
                 answer = cursor.execute(f'''SELECT answ_{i+1} FROM users_answer WHERE tg_user_id = ?''',(tg_user_id,))
@@ -165,24 +172,24 @@ async def final(msg: Message, state: FSMContext):
                 for group in hol_table:
                     if answ in hol_table[group]:
                         dict_prof[group] += 1
-            print(dict_prof)
+
 
             for group in dict_prof:
-                if dict_prof[group]==max(dict_prof.values()):
+                if dict_prof[group] == max(dict_prof.values()):
                     max_ball_group.append(group)
             # print(max_ball_group) полученная(ые) группы с макс баллом
             if len(max_ball_group)==1:
-                await msg.answer('По итогам теста мы определили 1 тип личности, с профессиями, которые могут вам подойти.')
+                await callback.message.answer('По итогам теста мы определили 1 тип личности, с профессиями, которые могут вам подойти.')
             elif len(max_ball_group)>1:
-                await msg.answer(f'По итогам теста мы определили {len(max_ball_group)} типов личности, с профессиями, '
+                await callback.message.answer(f'По итогам теста мы определили {len(max_ball_group)} типов личности, с профессиями, '
                                  f'которые могут вам подойти.')
             await asyncio.sleep(5)
-            await msg.answer('Вот названия типов и что они означают')
+            await callback.message.answer('Вот названия типов и что они означают')
             await asyncio.sleep(3)
             # if 'social' in max_ball_group:
             #     print(';2561651d5f1dsf')
             if 'realistic' in max_ball_group:
-                await msg.answer(f'<strong>Реалистический</strong> – предпочитает работать с вещами, а не с '
+                await callback.message.answer(f'<strong>Реалистический</strong> – предпочитает работать с вещами, а не с '
                                  f'людьми. Это несоциальный, эмоционально-стабильный тип.'
                                  f'. Занимается конкретными объектами и их использованием (вещи, инструменты, техника). Хорошо'
                                  f'приспосабливается к обстановке, пластичен, трудолюбив.'
@@ -190,7 +197,7 @@ async def final(msg: Message, state: FSMContext):
                                  f'силы, ловкости, подвижности, хорошей координации движений, навыков практической работы.')
                 await asyncio.sleep(10)
             if 'intelligent' in max_ball_group:
-                await msg.answer(f'<strong>Интеллектуальный (исследовательский)</strong> – ориентирован на труд '
+                await callback.message.answer(f'<strong>Интеллектуальный (исследовательский)</strong> – ориентирован на труд '
                                  f'с идеями и с вещами (объектами). Присуща как пластичность, так и '
                                  f'ригидность в действиях. '
                                  f'Отличается целеустремленностью, настойчивостью, терпеливостью. '
@@ -200,7 +207,7 @@ async def final(msg: Message, state: FSMContext):
                                  f'идеи. Они часто выбирают научную и исследовательскую работу. Им '
                                  f'нужна свобода для творчества.')
             if 'social' in max_ball_group:
-                await msg.answer(f' <strong>Социальный</strong> – ориентирован на общение, взаимодействие с другими людьми.'
+                await callback.message.answer(f' <strong>Социальный</strong> – ориентирован на общение, взаимодействие с другими людьми.'
                                  f'Предпочитает работать с людьми, а не с вещами. '
                                  f'Развитые вербальные способности, повышенная'
                                  f'приспособляемость «пластичность» к меняющейся обстановке. '
@@ -209,7 +216,7 @@ async def final(msg: Message, state: FSMContext):
                                  f' прийти на помощь.')
                 await asyncio.sleep(10)
             if 'conventional' in max_ball_group:
-                await msg.answer(f'<strong>Конвенциальный</strong> – отдает предпочтение четко '
+                await callback.message.answer(f'<strong>Конвенциальный</strong> – отдает предпочтение четко '
                                  f'структурированной деятельности. Выбирает такие цели и задачи, которые '
                                  f'четко подтверждаются обществом и обычаями. '
                                  f'Характерны консерватизм, '
@@ -223,7 +230,7 @@ async def final(msg: Message, state: FSMContext):
                                  f'знаками).')
                 await asyncio.sleep(10)
             if 'enterprising' in max_ball_group:
-                await msg.answer(f' <strong>Предприимчивый</strong> – выбирает цели и задачи, которые позволяют '
+                await callback.message.answer(f' <strong>Предприимчивый</strong> – выбирает цели и задачи, которые позволяют '
                                  f'ему проявить энергию, энтузиазм. Сочетаются импульсивность и '
                                  f'холодный расчет. Наделен как вербальными, так и невербальными '
                                  f'способностями, обладает интуицией и навыками эффективного '
@@ -234,7 +241,7 @@ async def final(msg: Message, state: FSMContext):
                                  f'активны, готовы рисковать, ищут острые ощущения. Любят и умеют общаться.')
                 await asyncio.sleep(10)
             if 'artistic' in max_ball_group:
-                await msg.answer(f'<strong>Артистический</strong> – сложный взгляд на жизнь, гибкость и '
+                await callback.message.answer(f'<strong>Артистический</strong> – сложный взгляд на жизнь, гибкость и '
                                  f'независимость в принятии решений.Предпочитает занятия творческого характера. '
                                  f'Преобладают вербальные способности. Для этого типа характерны '
                                  f'исключительные способности восприятия и моторики, высокая '
@@ -251,7 +258,7 @@ async def final(msg: Message, state: FSMContext):
                     text=f'{max_ball_group[i]}',
                     callback_data=f'group_{max_ball_group[i]}'
                 ))
-            await msg.answer('Выберите тип, который вам больше подходит',
+            await callback.message.answer('Выберите тип, который вам больше подходит',
                              reply_markup=keyboard.as_markup(resize_keyboard=True))
 
 
@@ -296,14 +303,9 @@ async def list_profs(callback: types.CallbackQuery, state:FSMContext):
     start_btns = int(callback.data.split('_')[1])
     end_btns = int(callback.data.split('_')[2])
     current_page = (end_btns//5)
-    print(start_btns)
-    print(end_btns)
-    print('len_group: ', len_group)
-    print('max_pages ', max_pages)
-    print('max_pages*5 ', max_pages*5)
 
     if end_btns <= len_group and start_btns >= 0:
-        print('if')
+
         if current_page == 1:
 
             keyboard = InlineKeyboardBuilder()
@@ -374,7 +376,7 @@ async def list_profs(callback: types.CallbackQuery, state:FSMContext):
             await callback.message.answer(text=f'Страница: {current_page} из {max_pages}',
                                           reply_markup=keyboard.as_markup(resize_keyboards=False))
     elif (current_page/max_pages) == 1:
-        print('elif 1')
+
         keyboard = InlineKeyboardBuilder()
 
         for key in range((end_btns-5), len_group+1):
@@ -420,13 +422,89 @@ async def print_college(callback: types.CallbackQuery, state: FSMContext):
 
     print(info_user)
     town_list = work_with_db.find_college(info_user[1], work_with_db.search_city(info_user[0]))
+    if town_list is None:
+        await callback.message.answer(text='В выбранном вами городе нету колледжей с данными профессий')
+    else:
+        keyboard = InlineKeyboardBuilder()
+        for college in town_list:
+            print(town_list)
+            print(college)
+            print(college.split(":")[1]+college.split(":")[2])
+            keyboard.row(InlineKeyboardButton(
+                text=f'{college.split(":")[0].split(".")[2]}',
+                url=f'{college.split(":")[1]+":"+college.split(":")[2]}'
+            ))
+        await callback.message.answer(text=f'Вот список колледжей в выбранном городе по вашей професии:',
+                                      reply_markup=keyboard.as_markup(resize_keyboards=False))
+        # await callback.message.delete()
+
+
+@router.message(WaitList.start, Command("menu"))
+async def menu(msg: Message, state: FSMContext):
+    await msg.answer('Вы только начали нажмите кнопку "Пройти тест", или допройдите тест')
+
+@router.message(WaitList.menu, Command("menu"))
+async def menu(msg: Message, state: FSMContext):
+    keyboard = ReplyKeyboardBuilder()
+    for i in range(1):
+     keyboard.add(
+types.KeyboardButton(text='Перепройти тест'),
+        types.KeyboardButton(text='Посмотреть результат теста'),
+        types.KeyboardButton(text='Показать город'),
+        types.KeyboardButton(text='Назад')
+        )
+    keyboard.adjust(1)
+    await msg.answer("Выберите действие", reply_markup=keyboard.as_markup(resize_keyboards=True))
+
+
+@router.message(WaitList.menu, F.text == "Показать город")
+async def city(msg: types.Message):
+    with sqlite3.connect('database/users.db') as connection:
+        cursor = connection.cursor()
+        city_from_user_id = msg.from_user.id
+        cursor.execute('''SELECT city FROM users_answer WHERE tg_user_id = ?''', (city_from_user_id,))
+        city_result = cursor.fetchone()
+        if city_result:
+            city_from_user = city_result[0]
+            await msg.answer(text=f'Выбранный вами город: {city_from_user}')
+
     keyboard = InlineKeyboardBuilder()
-    for college in town_list:
-        print(college)
-        print(college.split(":")[1]+college.split(":")[2])
-        keyboard.row(InlineKeyboardButton(
-            text=f'{college.split(":")[0]}',
-            url=f'{college.split(":")[1]+":"+college.split(":")[2]}'
-        ))
-    await callback.message.answer(text=f'Вот список колледжей в выбранном городе по вашей професии:',
-                                  reply_markup=keyboard.as_markup(resize_keyboards=False))
+    keyboard.add(InlineKeyboardButton(
+        text="Изменить город",
+        callback_data="change_city_cb")
+    )
+    await msg.answer('Изменить город?', reply_markup=keyboard.as_markup(resize_keyboard=True))
+
+
+@router.callback_query(WaitList.menu, F.data == 'change_city_cb')
+async def change_city_callback(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.answer('Введите город')
+    await state.set_state(WaitList.change_city)
+
+
+
+
+
+
+@router.message(F.text == "Перепройти тест")
+async def test_restart(msg: Message, state: FSMContext):
+    keyboard = InlineKeyboardBuilder()
+    keyboard.add(InlineKeyboardButton(
+        text="Подтвердить",
+        callback_data="Test")
+    )
+    await state.set_state(WaitList.start)
+    dict_num_ques = {}
+    dict_num_ques['number_question'] = 0
+    await state.set_data(dict_num_ques)
+    await msg.answer("Вы готовы перепройти тест", reply_markup=keyboard.as_markup(resize_keybiards=True))
+
+
+@router.message(F.text == "Посмотреть результат теста")
+async def test_restart(msg: Message):
+    keyboard = InlineKeyboardBuilder()
+    keyboard.add(InlineKeyboardButton(
+        text="Посмотреть",
+        callback_data="result_test")
+    )
+    await msg.answer("Вы хотите посмотреть результаты теста", reply_markup=keyboard.as_markup(resize_keyboards=True))
